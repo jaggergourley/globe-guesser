77fcba27290244736b23459fabff26f6624e4ec7
Jagger Gourley <jaggergourley@gmail.com>
2025-01-15 14:34:47 -0600

    Starting from scratch for globe

--- script.js
+++ script.js
@@ -1,115 +1,14 @@
-import { initBuffers } from "./init-buffers.js";
-import { drawScene } from "./draw-scene.js";
+const radius = 1;
+const latSteps = 20;
+const longSteps = 20;
 
-let cubeRotation = 0.0;
-let deltaTime = 0;
+let vertices = [];
 
-main();
-
-function main() {
-  const canvas = document.querySelector("#gl-canvas");
-
-  const gl = canvas.getContext("webgl");
-
-  if (gl == null) {
-    alert("Unable to initialize WebGL.");
-    return;
+for (let latitude = 0; latitude <= latSteps; latitude++) {
+  let theta = (latitude * Math.PI) / latSteps;
+  for (let longitude = 0; longitude <= longSteps; longitude++) {
+    let phi = (longitude * 2 * Math.PI) / longSteps;
   }
 
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  const vsSource = `
-  attribute vec4 aVertexPosition;
-  attribute vec4 aVertexColor;
-
-  uniform mat4 uModelViewMatrix;
-  uniform mat4 uProjectionMatrix;
-
-  varying lowp vec4 vColor;
-
-  void main(void) {
-  gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-  vColor = aVertexColor;
-  }
-  `;
-  const fsSource = `
-  varying lowp vec4 vColor;
-
-  void main(void) {
-  gl_FragColor = vColor;
-  }
-  `;
-
-  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
-
-  const programInfo = {
-    program: shaderProgram,
-    attribLocations: {
-      vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
-      vertexColor: gl.getAttribLocation(shaderProgram, "aVertexColor"),
-    },
-    uniformLocations: {
-      projectionMatrix: gl.getUniformLocation(
-        shaderProgram,
-        "uProjectionMatrix"
-      ),
-      modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
-    },
-  };
-
-  const buffers = initBuffers(gl);
-
-  let then = 0;
-
-  function render(now) {
-    now *= 0.001;
-    deltaTime = now - then;
-    then = now;
-
-    drawScene(gl, programInfo, buffers, cubeRotation);
-    cubeRotation += deltaTime;
-
-    requestAnimationFrame(render);
-  }
-  requestAnimationFrame(render);
-}
-
-function initShaderProgram(gl, vsSource, fsSource) {
-  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
-  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
-
-  const shaderProgram = gl.createProgram();
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-  gl.linkProgram(shaderProgram);
-
-  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    alert(
-      `Unable to initialize the shader program: ${gl.getProgramInfoLog(
-        shaderProgram
-      )}`
-    );
-    return null;
-  }
-
-  return shaderProgram;
-}
-
-function loadShader(gl, type, source) {
-  const shader = gl.createShader(type);
-
-  gl.shaderSource(shader, source);
-
-  gl.compileShader(shader);
-
-  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    alert(
-      `An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`
-    );
-    gl.deleteShader(shader);
-    return null;
-  }
-
-  return shader;
+  let x = radius * Math;
 }
