bc97927f290cc1c3eff3b559efe5199e616987d3
Jagger Gourley <jaggergourley@gmail.com>
2025-01-16 11:07:43 -0600

    Progressing on rendering vertices of sphere

--- script.js
+++ script.js
@@ -1,14 +1,85 @@
-const radius = 1;
-const latSteps = 20;
-const longSteps = 20;
+main();
 
-let vertices = [];
+function main() {
+  const canvas = document.querySelector("#gl-canvas");
+  const gl = canvas.getContext("webgl");
 
-for (let latitude = 0; latitude <= latSteps; latitude++) {
-  let theta = (latitude * Math.PI) / latSteps;
-  for (let longitude = 0; longitude <= longSteps; longitude++) {
-    let phi = (longitude * 2 * Math.PI) / longSteps;
+  if (!gl) {
+    alert("Unable to initialize WebGL.");
+    return;
   }
 
-  let x = radius * Math;
+  const vertexShaderSource = `
+    attribute vec4 aVertexPosition;
+    uniform mat4 uProjectionMatrix;
+    uniform mat4 uViewMatrix;
+    
+    void main() {
+      gl_Position = uProjectionMatrix * uViewMatrix * aVertexPosition;
+      gl_PointSize = 5.0;
+    }
+  `;
+
+  const fragmentShaderSource = `
+    precision mediump float;
+    void main() {
+      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+  `;
+
+  gl.clearColor(0.5, 0.5, 0.5, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  const radius = 1;
+  const latitudeBands = 20;
+  const longitudeBands = 20;
+
+  let vertices = [];
+
+  for (let latitude = 0; latitude <= latitudeBands; latitude++) {
+    let theta = (latitude * Math.PI) / latitudeBands;
+    for (let longitude = 0; longitude <= longitudeBands; longitude++) {
+      let phi = (longitude * 2 * Math.PI) / longitudeBands;
+
+      const x = radius * Math.sin(theta) * Math.cos(phi);
+      const y = radius * Math.sin(theta) * Math.sin(phi);
+      const z = radius * Math.sin(phi);
+
+      vertices.push(x, y, z);
+    }
+  }
+
+  console.log(vertices);
+
+  const vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 }
+
+function initShaderProgram(gl, vertexShaderSource, fragmentShaderSource) {
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+  const fragmentShader = loadShader(
+    gl,
+    gl.FRAGMENT_SHADER,
+    fragmentShaderSource
+  );
+
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert(
+      `Unable to initialize the shader program: ${gl.getProgramInfoLog(
+        shaderProgram
+      )}`
+    );
+    return null;
+  }
+
+  return shaderProgram;
+}
+
+function loadShader() {}
